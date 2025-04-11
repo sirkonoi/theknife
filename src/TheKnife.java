0080d5d04785639dacf03befb010fc46336745fe
sirkonoi <mattia.rotteri@outlook.com>
2025-04-11 12:08:22 +0200

    main

--- src/TheKnife.java
+++ src/TheKnife.java
@@ -7,16 +7,17 @@ public class TheKnife {
     public static Scanner sc = new Scanner(System.in);
     public static Utente user;
 
-    public static void pulisci() {  
-        System.out.print("\033[H\033[2J");  
-        System.out.flush();  
-    }    
+    public static void pulisci() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static void menu() throws IOException {
         System.out.println("Menu':");
-        System.out.println("1 - Entra come Guest");        
+        System.out.println("1 - Entra come Guest");
         System.out.println("2 - Login");
-        System.out.println("3 - Registrati");  
-        
+        System.out.println("3 - Registrati");
+
         String m;
         do {
             m = sc.nextLine();
@@ -24,84 +25,105 @@ public class TheKnife {
                 System.out.println("Errore. Inserisci un'opzione valida.");
             }
         } while (!(m.equals("1") || m.equals("2") || m.equals("3")));
-        
-        switch(m) {
+
+        switch (m) {
             case "1":
-            pulisci();
-            Guest u = new Guest();
-            System.out.println("Benvenuto Guest");
-            //chiama app();
-            break;            
+                pulisci();
+                Guest u = new Guest();
+                System.out.println("Benvenuto Guest");
+                // chiama app();
+                break;
             case "2":
-            while (true) {
-                try {
-                    pulisci();
-                    System.out.print("Login - The Knife\nInserisci il tuo username: ");
-                    String username = sc.nextLine();
-                    System.out.print("Inserisci la password: ");
-                    String psw = sc.nextLine();
-        
-                    user = Utente.login(username, psw);
-                    break; // login riuscito allora esce dal ciclo
-                } catch (ErroreLogin e) {
-                    pulisci();
-                    System.out.println("Errore. Login non riuscito! Vuoi: \n1 - Tornare al menu' \n2 - Ritentare il login");
-                    String scelta = sc.nextLine();
-                    if (scelta.equals("1")) {
-                        menu(); // torna al menu
-                        break;
+                while (true) {
+                    try {
+                        pulisci();
+                        System.out.print("Login - The Knife\nInserisci il tuo username: ");
+                        String username = sc.nextLine();
+                        System.out.print("Inserisci la password: ");
+                        String psw = sc.nextLine();
+
+                        user = Utente.login(username, psw);
+                        break; // login riuscito allora esce dal ciclo
+                    } catch (ErroreLogin e) {
+                        pulisci();
+                        System.out.println(
+                                "Errore. Login non riuscito! Vuoi: \n1 - Tornare al menu' \n2 - Ritentare il login");
+                        String scelta = sc.nextLine();
+                        if (scelta.equals("1")) {
+                            menu(); // torna al menu
+                            break;
+                        }
+                        // se 2, viene ripetuto il ciclo
                     }
-                    // se 2, viene ripetuto il ciclo
+                }
+                pulisci();
+                System.out.println("loggato.. as " + user.getUsername());
+                break;
+            // app();
+            case "3":
+                while (true) {
+                    String username = "";
+
+                    pulisci();
+                    System.out.println("TheKnife - Registrazione");
+                    
+                    while (true) {
+                        System.out.print("Inserisci il tuo username: ");
+                        username = sc.nextLine();
+                    
+                        if (Utente.checkUser(username)) {
+                            System.out.println("Errore. Username già esistente. Riprova.");
+                        } else {
+                            break; // se l'username non esiste allora esce dal ciclo
+                        }
+                    }
+                        
+                        pulisci();
+                        System.out.print("Inserisci la tua password:");
+                        String psw = sc.nextLine();
+                        pulisci();
+                        System.out.print("Inserisci il tuo nome:");
+                        String nome = sc.nextLine();                        
+                        pulisci();
+                        System.out.print("Inserisci il tuo cognome:");
+                        String cognome = sc.nextLine();
+                        pulisci();
+                        System.out.print("Inserisci il tuo domicilio:");
+                        String domicilio = sc.nextLine();
+                        pulisci();
+                        System.out.print("Inserisci il tuo ruolo:");
+                        String ruolo = sc.nextLine();
+                        pulisci();
+                    try {                        
+                        if (ruolo.equals("utente")) {
+                            user = Utente.register(username, psw, nome, cognome, domicilio, "utente");
+                        } else {
+                            user = Ristoratore.register(username, psw, nome, cognome, domicilio, "ristoratore");
+                        }
+
+                        System.out.println(user.getRuolo());
+
+                    } catch(UserAlreadyExists e) {
+                        System.out.println("L'utente esiste gia'");
+                    }
+                    break;
                 }
             }
             pulisci();
-            System.out.println("loggato.. as " + user.getUsername());
-            break;        
-            //app();
-            case "3":
-            try {
-            System.out.println("TheKnife - Registrazione\nInserisci il tuo username:");
-            String username = sc.nextLine();
-            pulisci();
-            System.out.print("Inserisci la tua password:");
-            String psw = sc.nextLine();            
-            pulisci();
-            System.out.print("Inserisci il tuo nome:");
-            pulisci();
-            String nome = sc.nextLine();              
-            System.out.print("Inserisci il tuo cognome:");
-            String cognome = sc.nextLine();            
-            pulisci();
-            System.out.print("Inserisci il tuo domicilio:");
-            String domicilio = sc.nextLine();        
-            pulisci();      
-            System.out.print("Inserisci il tuo ruolo:");
-            String ruolo = sc.nextLine();
-            pulisci();
-            if(ruolo.equals("utente")) {
-                user = Utente.register(username, psw, nome, cognome, domicilio, "utente");
-            }
-            else{
-                user = Ristoratore.register(username, psw, nome, cognome, domicilio, "ristoratore");
-            }
-
-            System.out.println(user.getRuolo());
-
-            } catch(UserAlreadyExists e) {
-                System.out.println("L'utente esiste gia'");
-            }
-            break;
-
+            System.out.println(Utente.checkRuolo("Registrazione ok, " + user.getUsername() + " sei un " + user.getRuolo()));
         }
-    }    
+
     public static void main(String[] args) throws IOException, UserAlreadyExists, ErroreLogin {
         System.out.println("Benvenuto in TheKnife.");
 
-        /*System.out.println(Utente.checkUser("konoi"));
-        Utente.register("konod", "Ciao", "mattia", "rotteri", "Via Davide Plesa", "utente");
-        Utente u = Utente.login("konoi", "ciao1234");
-        System.out.println(u.getUsername());
-        System.out.println(Utente.checkRuolo("konoi"));*/
+        /*
+         * System.out.println(Utente.checkUser("konoi"));
+         * Utente.register("konod", "Ciao", "mattia", "rotteri", "Via Davide Plesa",
+         * "utente");
+         * Utente u = Utente.login("konoi", "ciao1234");
+         * System.out.println(u.getUsername());
+         * System.out.println(Utente.checkRuolo("konoi"));
+         */
         menu();
     }
 }
