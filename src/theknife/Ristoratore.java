5d72d40a57c0eec9c71ad07d87def7de62bc2d52
DavidePlesa <plesadavide@gmail.com>
2025-04-11 10:49:38 +0200

    Ristorante

--- src/theknife/Ristoratore.java
+++ src/theknife/Ristoratore.java
@@ -1,5 +1,37 @@
-/*package theknife;
+package theknife;
 
 public class Ristoratore extends Utente {
-    public Ristoratore()
-}*/
+    //campi
+    private String username;
+    private LinkedList<Ristorante> ristorante;
+
+    //costruttore
+    public Ristoratore() {
+        super();
+        LinkedList<Ristorante> ristorante = new LinkedList<Ristorante>;
+        this.ruolo = "ristoratore";
+    }
+
+    //metodi
+    public aggiungiRistorante(String s) {
+        /*
+        LinkedList<List<String>> user = getUsers();
+        for (List<String> user : users) {
+            if (user.get(0).equals(username) && user.get(5).equals("Ristoratore")) {
+                break;
+            }
+        }
+        */
+        FileWriter fr = new FileWriter("data" + sep + "ristoratori.csv", true);
+        try {
+            fr.write("\n" + username + "," + s);
+            fr.close();
+        }
+
+        catch(IOException e) {
+            System.out.println("Errore...");
+        }
+    }
+
+
+}
